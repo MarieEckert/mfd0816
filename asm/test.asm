e1ef760fb0340e47f749827e6f165eb6ba45abc2
Marie <marie@ha.si>
2025-01-03 05:32:03 +0100

    asm: update test.asm to get rid of constant expressions

--- asm/test.asm
+++ asm/test.asm
@@ -1,16 +1,14 @@
 addressing absolute
 
-define test, 123
-
 section code at 0x1100
-	_entry:		mov 0x1000, sp	; initialise stack pointer
-				ld  acl, [sp]	; load the word from the top of the
-								; stack into acl
-				ld  bcl, [[0x1330]]
+	_entry:	mov	0x1000,	sp			; initialise stack pointer
+			ld	acl,	[sp]		; load the word from the top of the
+									; stack into acl
+			ld	bcl,	[[0x1330]]
 
 section data at 0xd000
-	buffer:			times 1024 db 0
-	buffer_size:	dw _here - buffer
+define bufferSize,	1024
+	buffer:			times bufferSize db 0
 	text:			"hello, world! \\ \" \\ \\\"" db 0
 
 section reset_vector at 0xfffe
