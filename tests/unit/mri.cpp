d464a3941336d487b2b4d4bea8ae7b0817a46dd0
Marie <marie@ha.si>
2025-07-21 20:44:03 +0200

    tests: finish mri generation & validity test

--- tests/unit/mri.cpp
+++ tests/unit/mri.cpp
@@ -1,6 +1,7 @@
-#include <cstdint>
+#include <algorithm>
 #include <filesystem>
 #include <fstream>
+#include <ranges>
 #include <vector>
 
 #include <unistd.h>
@@ -20,7 +21,7 @@
 
 using namespace mfdasm;
 
-TEST_CASE("generate & parse mri") {
+TEST_CASE("generate & validate mri") {
 	impl::Assembler asem;
 	Result<None, impl::AsmError> asm_res = asem.parseLines(R"(
 	section garbage at 0x0000
@@ -55,8 +56,64 @@ TEST_CASE("generate & parse mri") {
 
 	std::ifstream stream(tmp_file);
 	REQUIRE(stream.is_open());
-	std::vector<u8> mri_bytes(
+	const std::vector<u8> mri_bytes(
 		(std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
-	std::vector<u8> parsed_mri = mfdemu::parseMRIFromBytes(mri_bytes);
-	REQUIRE(parsed_mri.size() == UINT16_MAX);
+	stream.close();
+
+	/* ensure that the header and section table are correct */
+	CHECK(/* MAGIC */
+		  std::ranges::equal(
+			  mri_bytes | std::views::take(4), std::vector<u8>{'M', 'R', 'I', 0x00}));
+	CHECK(/* VERSION */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(4) | std::views::take(2), std::vector<u8>{0x01, 0x00}));
+	CHECK(/* FLAGS (compact)*/
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(6) | std::views::take(2), std::vector<u8>{0x01, 0x00}));
+	CHECK(/* FILESIZE */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(8) | std::views::take(4),
+			  shared::intops::u64ToBytes(mri_bytes.size()) | std::views::drop(4)));
+	CHECK(/* DATA OFFSET */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(12) | std::views::take(4),
+			  shared::intops::u64ToBytes(sizeof(shared::mri_types::Header)) | std::views::drop(4)));
+	CHECK(/* SECTION TABLE: ENTRY COUNT*/
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(16) | std::views::take(4),
+			  std::vector<u8>{0x00, 0x00, 0x00, 0x03}));
+	/* SECTION TABLE ENTRIES*/
+	CHECK(/* 01 */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(20) | std::views::take(8),
+			  std::vector<u8>{
+				  0x00, 0x00, 0x00, 0x2c, /* FILE OFFSET */
+				  0x00, 0x00,			  /* LOAD ADDRESS */
+				  0x02, 0x00,			  /* LENGTH */
+			  }));
+	CHECK(/* 02 */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(28) | std::views::take(8),
+			  std::vector<u8>{
+				  0x00, 0x00, 0x02, 0x2c, /* FILE OFFSET */
+				  0x11, 0x00,			  /* LOAD ADDRESS */
+				  0x00, 0x0a,			  /* LENGTH */
+			  }));
+	CHECK(/* 03 */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(36) | std::views::take(8),
+			  std::vector<u8>{
+				  0x00, 0x00, 0x02, 0x36, /* FILE OFFSET */
+				  0xff, 0xe4,			  /* LOAD ADDRESS */
+				  0x00, 0x1c,			  /* LENGTH */
+			  }));
+	/* END SECTION TABLE ENTRIES */
+
+	/* ensure that some of the expected data is there */
+	CHECK(/* BULLSHIT DATA */
+		  std::ranges::equal(
+			  mri_bytes | std::views::drop(0x2c) | std::views::take(512),
+			  std::vector<u8>(512, 0xff)));
+
+	std::remove(tmp_file.c_str());
 }
