425139e4b9fb049e4321bf0d1737fe72cf29bafb
Marie <marie@ha.si>
2025-07-26 16:36:11 +0200

    emu: remove debug logs from bin instruction

--- emu/mfdemu/impl/cpu.cpp
+++ emu/mfdemu/impl/cpu.cpp
@@ -711,9 +711,7 @@ void Cpu::execInstBIN() {
 	switch(m_stateStep) {
 		GET_OPERAND_MOVE_TO_STASH(m_operand1, m_stash1, GET_OPERAND2, 0, STASH_OPERAND1)
 	GET_OPERAND2:
-		logDebug() << "get operand 2\n";
 		if(m_operand2.mode.indirect) {
-			logDebug() << "indirect\n";
 			m_addressBusAddress = m_operand2.mode.is_register
 									  ? getRegister((m_operand1.value & 0xFF00) >> 8)
 									  : m_operand2.value;
@@ -721,22 +719,18 @@ void Cpu::execInstBIN() {
 			newState(CpuState::ABUS_READ);
 			break;
 		}
-		logDebug() << "potentially direct\n";
 		m_stash2 = m_operand2.mode.is_register ? getRegister((m_operand1.value & 0xFF00) >> 8)
 											   : m_operand2.value;
 		goto READ_LOOP;
 	case STASH_OPERAND2:
-		logDebug() << "stash operand 2\n";
 		m_stash2 = m_addressBusInput;
 	case READ_LOOP:
 	READ_LOOP:
-		logDebug() << "read GIO\n";
 		m_ioBusAddress = m_stash1;
 		m_stateStep = STORE;
 		newState(CpuState::GIO_READ);
 		break;
 	case STORE:
-		logDebug() << "store read value\n";
 		m_stash1 += 2;
 		setRegister(REGISTER_AL, GET_LOW(m_regACL) - 1);
 		m_stateStep = (GET_LOW(m_regACL) == 0) ? EXEC_INST_STEP_INC_IP : READ_LOOP;
@@ -752,7 +746,6 @@ void Cpu::execInstBIN() {
 			break;
 		}
 
-		logDebug() << "write value to address bus\n";
 		m_addressBusAddress = m_stash2;
 		m_stash2 += 2;
 		m_addressBusOutput = m_ioBusInput;
