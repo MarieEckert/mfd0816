9de793ad3325cb66e6feb1a20f4ac301dd05e6d4
Marie <marie@ha.si>
2025-10-16 21:03:24 +0200

    scripts: add helpers.sh and rely on it

--- scripts/run-clang-format.sh
+++ scripts/run-clang-format.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source $(dirname "$0")/helpers.sh
 
 DIRECTORIES=(
 	asm/
@@ -9,18 +10,23 @@ DIRECTORIES=(
 
 format_files=()
 
+log_info "$(clang-format --version)"
+
 if [ -n "$1" ]; then
+	log_info "Formatting files changed compared to origin/main"
 	mapfile -t format_files < <(
 		git diff --name-only origin/main...HEAD -- "${DIRECTORIES[@]}" \
 		| grep "\.\(cpp\|hpp\)$" || true
 	)
 else
+	log_info "Formatting all files"
 	mapfile -t format_files < <(
-		find ${DIRECTORIES[@]} -type f -regex '.*\.\(cpp\|hpp\)$'
+		find_cpp_files ${DIRECTORIES[@]}
 	)
 fi
 
 for file in "${format_files[@]}"; do
-	echo "  formatting ${file}"
-	clang-format -i "${file}"
+	clang-format -i "${file}" \
+		&& print_status "ok" "formatted ${file}" \
+		|| print_status "fail" "failed to format ${file}"
 done
