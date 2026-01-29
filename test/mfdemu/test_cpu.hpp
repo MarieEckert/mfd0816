a23b1c27607b1f2abf040f0f6431996122e1f2e5
Marie <marie@ha.si>
2026-01-29 16:49:50 +0100

    tests: update interrupt test
    * Adjust & cleanup test logic.
    * Add IRET and INT testing.

--- test/mfdemu/test_cpu.hpp
+++ test/mfdemu/test_cpu.hpp
@@ -24,5 +24,7 @@ class CpuTest : public Cpu {
 	u16 &m_ioBusInput = Cpu::m_ioBusInput;
 	u16 &m_ioBusOutput = Cpu::m_ioBusOutput;
 	u16 &m_ioBusAddress = Cpu::m_ioBusAddress;
+
+	std::stack<CpuState, std::vector<CpuState>> &m_state = Cpu::m_state;
 };
 }  // namespace test::mfdemu
